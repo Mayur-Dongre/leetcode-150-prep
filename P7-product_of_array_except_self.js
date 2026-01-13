4a17beab523ab922e71474bd14bb73cd08dd9937
Mayur Dongre <137223005+Mayur-Dongre@users.noreply.github.com>
2026-01-13 17:33:13 +0530

    solution to P7-product_of_array_except_self

--- P7-product_of_array_except_self.js
+++ P7-product_of_array_except_self.js
@@ -7,24 +7,24 @@
 // Input: nums = [1,2,3,4]
 // Output: [24,12,8,6]
 
-const productExceptSelf = function(nums) {
-const n = nums.length;
-const res = new Array(n).fill(1);
+const productExceptSelf = function (nums) {
+	const n = nums.length;
+	const res = new Array(n).fill(1);
 
-// Prefix products
-let prefix = 1;
-for (let i = 0; i < n; i++) {
-res[i] = prefix;
-prefix *= nums[i];
-}
+	// Prefix products
+	let prefix = 1;
+	for (let i = 0; i < n; i++) {
+		res[i] = prefix;
+		prefix *= nums[i];
+	}
 
-// Suffix products (multiplied into prefix result)
-let suffix = 1;
-for (let i = n - 1; i >= 0; i--) {
-res[i] *= suffix;
-suffix *= nums[i];
-}
-return res;
+	// Suffix products (multiplied into prefix result)
+	let suffix = 1;
+	for (let i = n - 1; i >= 0; i--) {
+		res[i] *= suffix;
+		suffix *= nums[i];
+	}
+	return res;
 };
 
 /////////////////////////////////////////////////
